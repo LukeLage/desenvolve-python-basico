a96f9f848080b8d70f22498beb26ff6fa30115d1
LukeLage <100864023+LukeLage@users.noreply.github.com>
2024-02-27 16:41:34 -0300

    Add files via upload

---
+++ Modulo 2/aula2_questao2.py
@@ -0,0 +1,17 @@
+texto = 'O resultado é'
+num1 = 10
+num2 = 3.5
+
+soma = num1 + num2
+
+print(texto)
+print(type(texto))
+
+print(num1)
+print(type(num1))
+
+print(num2)
+print(type(num2))
+
+print(soma)
+print(type(soma))
---
+++ Modulo 2/aula2_questao4.py
@@ -0,0 +1,5 @@
+deposito = 500
+
+juros_compostos = deposito * ((1 + 0.01) ** 3)
+
+print('Após três meses meu novo saldo é de: R$', juros_compostos)
