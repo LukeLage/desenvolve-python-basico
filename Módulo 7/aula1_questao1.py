cce914af14eab3c211c75959b7634a5b0ab96e92
LukeLage <100864023+LukeLage@users.noreply.github.com>
2024-06-19 13:17:37 -0300

    Update aula1_questao1.py

--- Módulo 7/aula1_questao1.py
+++ Módulo 7/aula1_questao1.py
@@ -1,1 +1,9 @@
 # Escreva um programa que solicita o nome do usuário e o imprime em forma de escada, como indicado no exemplo a seguir.
+
+nome = input('Digite seu nome: \n')
+contador = ''
+
+for letra in nome:
+    contador += letra
+    print(contador)
+    
