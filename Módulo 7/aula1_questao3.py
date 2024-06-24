a4226290e0f2b3961e8bb6b5edbaed51fb18332e
LukeLage <100864023+LukeLage@users.noreply.github.com>
2024-06-24 18:18:51 -0300

    Update aula1_questao3.py

--- Módulo 7/aula1_questao3.py
+++ Módulo 7/aula1_questao3.py
@@ -1,1 +1,19 @@
 # Escreva um script que dado uma frase conta os espaços em branco.
+
+
+def contar_espacos_em_branco(frase):
+    contagem_espacos = 0
+    for caractere in frase:
+        if caractere == " ":
+            contagem_espacos += 1
+    return contagem_espacos
+
+
+# Obter a frase do usuário
+frase = input("Digite uma frase: \n")
+
+# Contar os espaços em branco
+numero_espacos = contar_espacos_em_branco(frase)
+
+# Imprimir o resultado
+print(f"A frase contém {numero_espacos} espaços em branco.")
