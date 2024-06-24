98d6b66e08a63d16f3050f4c55c201ec038996d0
LukeLage <100864023+LukeLage@users.noreply.github.com>
2024-06-24 18:26:55 -0300

    Update aula2_questao2.py

--- Módulo 7/aula2_questao2.py
+++ Módulo 7/aula2_questao2.py
@@ -1,1 +1,31 @@
 # Desenvolva um programa que solicite ao usuário inserir uma frase e substitua todas as ocorrências de vogal por "*".
+
+# Desenvolva um programa que solicite ao usuário inserir uma frase e substitua todas as ocorrências de vogal por "*".
+
+
+def substituir_vogais(frase):
+    # Vogais em minúsculo e maiúsculo
+    vogais = "aeiouAEIOU"
+
+    # Frase sem vogais
+    frase_sem_vogais = ""
+
+    # Percorrer cada caractere da frase
+    for caractere in frase:
+        # Verificar se o caractere é vogal
+        if caractere in vogais:
+            # Substituir vogal por "*"
+            frase_sem_vogais += "*"
+        else:
+            # Adicionar outros caracteres à frase sem vogais
+            frase_sem_vogais += caractere
+
+    return frase_sem_vogais
+
+
+# Obter a frase do usuário
+frase = input("Digite a frase que deseja alterar: \n")
+
+# Substituir vogais e imprimir resultado
+frase_sem_vogais = substituir_vogais(frase)
+print(f"Frase com vogais substituídas por '*': {frase_sem_vogais}")
