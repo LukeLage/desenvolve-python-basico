6d632b179db4b7a435d4cee9a4c1a32ca00e3270
LukeLage <100864023+LukeLage@users.noreply.github.com>
2024-06-24 18:08:53 -0300

    Update aula4_questao2.py

--- Módulo 6/aula4_questao2.py
+++ Módulo 6/aula4_questao2.py
@@ -1,3 +1,39 @@
-#Solicite uma frase do usuário e usando compreensão de listas imprima:
-#A lista de vogais da frase, ordenada alfabeticamente
-#A lista de consoantes da frase
+# Solicite uma frase do usuário e usando compreensão de listas imprima:
+# A lista de vogais da frase, ordenada alfabeticamente
+# A lista de consoantes da frase
+
+# Função que irá contar vogais e consoantes
+
+def processar_frase(frase):
+    frase_sem_espaco = frase.lower().replace(" ", "")
+    
+    # Contador de vogais e consoantes 
+    
+    vogais = []
+    consoantes = []
+
+    # Iterar sobre cada caractere da frase
+    for caractere in frase_sem_espaco:
+        if caractere in "aeiou":
+            vogais.append(caractere)
+        elif caractere.isalpha():
+            consoantes.append(caractere)
+
+    # Ordenar as listas alfabeticamente
+    
+    vogais.sort()
+    consoantes.sort()
+
+    # Imprimir as listas conforme pedido em atividade 
+    
+    print(f"Vogais: {vogais}")
+    print(f"Consoantes: {consoantes}")
+
+
+# Solicitar a frase do usuário
+
+frase = input("Digite uma frase: \n")
+
+# Processar a frase
+
+processar_frase(frase)
