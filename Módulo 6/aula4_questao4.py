485a9380825f7c3a5e7d979b8388bacf86d5bd79
LukeLage <100864023+LukeLage@users.noreply.github.com>
2024-06-24 17:49:22 -0300

    Update aula4_questao4.py

--- Módulo 6/aula4_questao4.py
+++ Módulo 6/aula4_questao4.py
@@ -1,8 +1,14 @@
-#Reescreva o código a seguir para construir a lista aprovados usando compreensão de listas, ou seja, eliminando o laço de repetição.
+# Reescreva o código a seguir para construir a lista aprovados usando compreensão de listas, ou seja, eliminando o laço de repetição.
 
-#alunos = ["Maria", "Jose", "Carla", "Sol"]
-#notas = [35, 50, 20, 80]
-#aprovados = []
-#for i in range(len(notas)):
+# alunos = ["Maria", "Jose", "Carla", "Sol"]
+# notas = [35, 50, 20, 80]
+# aprovados = []
+# for i in range(len(notas)):
 #    if notas[i] >= 60:
 #        aprovados.append(alunos[i])
+
+alunos = ['Maria', 'Jose', 'Carla', 'Sol']
+notas = [35, 50, 20, 80]
+aprovados = [aluno for aluno, nota in zip(alunos, notas) if nota >= 60]
+
+print(f"Lista de aprovados: {aprovados}")
